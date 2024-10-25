2c75199d6bbc279a0150d65e0b5e068411be9e91
MITCHYUGAN <denmichigan123@gmail.com>
2024-10-25 20:17:28 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const screenShot = document.querySelectorAll(".screenshot")
+screenShot[1].style.backgroundColor = "#8767c9"
+screenShot[2].style.backgroundColor = "#569756"
+screenShot[3].style.backgroundColor = "#d158d3"
+screenShot[4].style.backgroundColor = "#ca8420"
+screenShot[5].style.backgroundColor = "#3b92dc"
